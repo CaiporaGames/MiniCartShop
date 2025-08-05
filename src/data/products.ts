5d4d5f2625c3db234705bb6abfc9db4ef52614d1
Caipora Games <caiporagames@yahoo.com>
2025-08-05 15:33:44 +0100

    update
    - Adding images links

--- src/data/products.ts
+++ src/data/products.ts
@@ -4,18 +4,18 @@ export const products = [
     id: 'prod_1',
     name: 'Stylish Hat',
     price: 1999, // in cents
-    image: '/images/hat.png',
+    image: 'https://images.pexels.com/photos/984619/pexels-photo-984619.jpeg',
   },
   {
     id: 'prod_2',
     name: 'Cool Shirt',
     price: 2999,
-    image: '/images/shirt.png',
+    image: 'https://images.unsplash.com/photo-1576566588028-4147f3842f27?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   {
     id: 'prod_3',
     name: 'Nice Pants',
     price: 3999,
-    image: '/images/pants.png',
+    image: 'https://images.unsplash.com/photo-1541099649105-f69ad21f3246?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
 ];
